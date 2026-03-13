a35d68dc206164425cf75903eed862588cce85cc
leynachen <leynachen@synology.com>
2026-03-13 14:35:03 +0800

    fix: system navigation bar color not match the app's NavigationBar background

--- app/src/main/java/com/leyna/nailmanagement/MainActivity.kt
+++ app/src/main/java/com/leyna/nailmanagement/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.leyna.nailmanagement
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.leyna.nailmanagement.ui.navigation.MainNavigation
@@ -17,6 +21,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val themePreferences = remember { ThemePreferences(this) }
+            val darkTheme = isSystemInDarkTheme()
+
+            DisposableEffect(darkTheme) {
+                enableEdgeToEdge(
+                    navigationBarStyle = if (darkTheme) {
+                        SystemBarStyle.dark(Color.TRANSPARENT)
+                    } else {
+                        SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                    }
+                )
+                onDispose {}
+            }
+
             NailManagementTheme(
                 dynamicColor = themePreferences.dynamicColor,
                 seedColorArgb = themePreferences.seedColorArgb
